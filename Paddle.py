662a64ce9669b26fa3b22eadebd707d4fac959fb
Sancho Montana <spencersnider9000@gmail.com>
2018-10-24 17:57:57 -0400

    Created a template for the paddles in a new Paddle.py file.

---
+++ Paddle.py
@@ -0,0 +1,14 @@
+class Paddle:
+    def __init__(self, x, y, height, girth, vel):
+        self.x = x
+        self.y = int(DISPLAY_HEIGHT / 2)
+        self.height = int(DISPLAY_HEIGHT / 5)
+        self.girth = self.height / 4
+        self.vel = 0
+
+    def move(self):
+            # TODO: set screen boundaries
+    def shrink(self):
+
+    def reset(self):
+
