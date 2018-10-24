8ff2efe198079bc1f5ada6086a78b82dc0d99673
Daniel Sennett <dansen402560@gmail.com>
2018-10-24 19:01:44 -0400

    Created two paddles in the display

--- Paddle.py
+++ Paddle.py
@@ -1,14 +1,16 @@
 class Paddle:
-    def __init__(self, x, y, height, girth, vel):
+    def __init__(self, x, y, height, vel):
         self.x = x
-        self.y = int(DISPLAY_HEIGHT / 2)
-        self.height = int(DISPLAY_HEIGHT / 5)
+        self.y = y
+        self.height = height
         self.girth = self.height / 4
         self.vel = 0
 
     def move(self):
-            # TODO: set screen boundaries
+        # TODO: set screen boundaries
+        pass
     def shrink(self):
-
+        pass
     def reset(self):
+        pass
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from Paddle import Paddle
+from Ball import Ball
 
 pygame.init()
 pygame.mouse.set_visible(False)
@@ -9,6 +11,11 @@ clock = pygame.time.Clock()
 
 FPS = 50 # Frames per second.
 
+WHITE = (255, 255, 255)
+
+P1 = Paddle(50, DISPLAY_HEIGHT / 2, DISPLAY_HEIGHT / 5, 0)
+P2 = Paddle(1150, DISPLAY_HEIGHT / 2, DISPLAY_HEIGHT / 5, 0)
+
 
 game_exit = False
 
@@ -18,3 +25,14 @@ while not game_exit:
             game_exit = True  # This allows the X in the corner to close the window.
         elif event.type == pygame.KEYDOWN:  # Everything in this elif statement has key input handling
             pass
+
+    #Draw Player 1s paddle
+    pygame.draw.rect(gameDisplay, WHITE, [P1.x - P1.girth / 2, P1.y - P1.height / 2, P1.girth, P1.height])
+
+    #Draw Player 2s paddle
+    pygame.draw.rect(gameDisplay, WHITE, [P2.x - P2.girth / 2, P2.y - P1.height / 2, P2.girth, P2.height])
+
+    #Update the game
+    pygame.display.update()
+    clock.tick(FPS)
+    
