c18db0e274fc47c56e70bb331edd1dbdbb5bb990
Sancho Montana <spencersnider9000@gmail.com>
2018-10-24 15:38:55 -0400

    Small update for input handing.

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 clock = pygame.time.Clock()
 
 FPS = 50 # Frames per second.
+
+
+game_exit = False
+
+while not game_exit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_exit = True  # This allows the X in the corner to close the window.
+        elif event.type == pygame.KEYDOWN:  # Everything in this elif statement has key input handling
+            pass
