b348bce128f9981edfc490c3132f5bff22ed6892
Sancho Montana <spencersnider9000@gmail.com>
2018-10-26 12:01:32 -0400

    Added the ball. The ball moves. Collision detection with the ball. However there is an error that is thrown sometimes when the y speed of the gets very high.

--- Ball.py
+++ Ball.py
@@ -1,7 +1,56 @@
+import Pong
+
 class Ball:
-    def __init__(self, x, y, x_vel, y_vel):
+    def __init__(self, x, y, radius, x_vel, y_vel):
         self.x = x
         self.y = y
+        self.radius = radius
         self.x_vel = x_vel
         self.y_vel = y_vel
-        
+        self.vel = (self.x_vel ** 2 + self.y_vel ** 2) ** 0.5
+        self.tolerance = 5  # Allowing a collision if the center of the ball is just off the paddle
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+    def collision(self, P1, P2):
+        if self.y - self.radius < 0 or self.y  + self.radius > Pong.DISPLAY_HEIGHT:
+            self.y_vel *= -1
+
+        if P1.y - P1.height / 2 - self.tolerance <= self.y <= P1.y + P1.height / 2 + self.tolerance:
+            if self.x - self.radius <= P1.x + P1.girth / 2 <= self.x and self.x_vel < 0:
+                P1_array = [P1.y - P1.height / 2 - self.tolerance + ((P1.height + 2 * self.tolerance) / 5 * i) for i in range(6)]
+                if P1_array[0] <= self.y <= P1_array[1]:
+                    self.y_vel -= self.vel / 2 - self.y_vel / 2
+                    self.x_vel = (self.vel ** 2 - self.y_vel ** 2) ** 0.5
+                elif P1_array[1] <= self.y <= P1_array[2]:
+                    self.y_vel -= self.vel / 3 - self.y_vel / 3
+                    self.x_vel = (self.vel ** 2 - self.y_vel ** 2) ** 0.5
+                elif P1_array[2] <= self.y <= P1_array[3]:
+                    self.x_vel = (self.vel ** 2 - self.y_vel ** 2) ** 0.5
+                elif P1_array[3] <= self.y <= P1_array[4]:
+                    self.y_vel += self.vel / 3 - self.y_vel / 3
+                    self.x_vel = (self.vel ** 2 - self.y_vel ** 2) ** 0.5
+                elif P1_array[4] <= self.y <= P1_array[5]:
+                    self.y_vel += self.vel / 2 - self.y_vel / 2
+                    self.x_vel = (self.vel ** 2 - self.y_vel ** 2) ** 0.5
+
+        if P2.y - P2.height / 2 - self.tolerance <= self.y <= P2.y + P2.height / 2 + self.tolerance:
+            if self.x + self.radius >= P2.x - P2.girth / 2 >= self.x and self.x_vel > 0:
+                P2_array = [P2.y - P2.height / 2 - self.tolerance + ((P2.height + 2 * self.tolerance) / 5 * i) for i in range(6)]
+                if P2_array[0] <= self.y <= P2_array[1]:
+                    self.y_vel -= self.vel / 2 - self.y_vel / 2
+                    self.x_vel = -((self.vel ** 2 - self.y_vel ** 2) ** 0.5)
+                elif P2_array[1] <= self.y <= P2_array[2]:
+                    self.y_vel -= self.vel / 3 - self.y_vel / 3
+                    self.x_vel = -((self.vel ** 2 - self.y_vel ** 2) ** 0.5)
+                elif P2_array[2] <= self.y <= P2_array[3]:
+                    self.x_vel = -((self.vel ** 2 - self.y_vel ** 2) ** 0.5)
+                elif P2_array[3] <= self.y <= P2_array[4]:
+                    self.y_vel += self.vel / 3 - self.y_vel / 3
+                    self.x_vel = -((self.vel ** 2 - self.y_vel ** 2) ** 0.5)
+                elif P2_array[4] <= self.y <= P2_array[5]:
+                    self.y_vel += self.vel / 2 - self.y_vel / 2
+                    self.x_vel = -((self.vel ** 2 - self.y_vel ** 2) ** 0.5)
+
--- Pong.py
+++ Pong.py
@@ -9,7 +9,8 @@ DISPLAY_WIDTH = 1600
 DISPLAY_HEIGHT = 800
 PADDLE_GAP = 60
 PADDLE_GIRTH = 20
-FPS = 50 # Frames per second.
+BALL_RADIUS = 15
+FPS = 80 # Frames per second.
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
@@ -23,6 +24,7 @@ def main():
 
     P1 = Paddle(PADDLE_GAP, DISPLAY_HEIGHT / 2, DISPLAY_HEIGHT / 4, PADDLE_GIRTH, 0)
     P2 = Paddle(DISPLAY_WIDTH - PADDLE_GAP, DISPLAY_HEIGHT / 2, DISPLAY_HEIGHT / 4, PADDLE_GIRTH, 0)
+    ball = Ball(DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2, BALL_RADIUS, 10, 0)
     game_exit = False
     while not game_exit:
         for event in pygame.event.get():
@@ -30,33 +32,36 @@ def main():
                 game_exit = True  # This allows the X in the corner to close the window.
             elif event.type == pygame.KEYDOWN:  # Everything in this elif statement has key input handling
                 if event.key == pygame.K_w:
-                    P1.vel -= 10
+                    P1.vel -= 6
                 elif event.key == pygame.K_s:
-                    P1.vel += 10
+                    P1.vel += 6
                 elif event.key == pygame.K_UP:
-                    P2.vel -= 10
+                    P2.vel -= 6
                 elif event.key == pygame.K_DOWN:
-                    P2.vel += 10
+                    P2.vel += 6
             elif event.type == pygame.KEYUP:  # Event handling when key is lifted.
                 if event.key == pygame.K_w:
-                    P1.vel += 10
+                    P1.vel += 6
                 elif event.key == pygame.K_s:
-                    P1.vel -= 10
+                    P1.vel -= 6
                 elif event.key == pygame.K_UP:
-                    P2.vel += 10
+                    P2.vel += 6
                 elif event.key == pygame.K_DOWN:
-                    P2.vel -= 10
+                    P2.vel -= 6
 
-        # Move Paddles
+        # Move Paddles and Ball
         P1.move()
         P2.move()
+        ball.move()
+        ball.collision(P1, P2)
 
         # Updates the black background, everything that is drawn should be after this.
         gameDisplay.fill(BLACK)
         # Draw paddles
         pygame.draw.rect(gameDisplay, WHITE, [P1.x - P1.girth / 2, P1.y - P1.height / 2, P1.girth, P1.height])
         pygame.draw.rect(gameDisplay, WHITE, [P2.x - P2.girth / 2, P2.y - P1.height / 2, P2.girth, P2.height])
-
+        # Draws the ball
+        pygame.draw.circle(gameDisplay, WHITE, (int(ball.x), int(ball.y)), ball.radius)
         # Update the game
         pygame.display.update()
         clock.tick(FPS)
